0db88ef88a9b3327ea7c5d10bc01a3ab0102d741
TripleThreat <200393376+CompTriplet@users.noreply.github.com>
2025-07-09 09:55:36 -0500

    Edited about me page and project page

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function takeToAbout(){
     //This allows the webpage to change
     location.href = "about.html";
 }
+function link(){
+    location.href = "about.html";
+}
+function where(){
+    document.getElementsByClassName("whereUsed").innerHTML = document.getElementsByClassName("options").value;
+}
